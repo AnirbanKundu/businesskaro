8e2f526ae88c396581b4813e2fe801170aae5040
anirban <anirban.kundu1981@gmail.com>
2015-06-06 18:16:16 -0700

    initial check-in

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name businesskaroApp
+ * @description
+ * # businesskaroApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('businesskaroApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
