303cdbfc7af5613a347bf764cd20f81903d12be7
Ritesh Mengji <ritesh.mengji@ge.com>
2015-06-14 21:06:13 -0700

    Folder structure change.. adding service-apis, service-imll and webapp maven projects

---
+++ businesskaro-webapp_1/src/main/java/com/businesskaro/Application.java
@@ -0,0 +1,35 @@
+package com.businesskaro;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+import com.businesskaro.dao.DBConnection;
+
+//The below annotation makes our class as web controller and spring will consider it 
+//when handling incoming web requests.
+@RestController
+@EnableAutoConfiguration
+@RequestMapping("/home")
+public class Application {
+
+	//the below annotation provides routing information.
+	@RequestMapping(method = RequestMethod.GET)
+    public String home() {
+        return "Hello World!";
+    }
+	/*
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+    */
+	public static void main(String[] args) throws Exception {  
+	    String webPort = System.getenv("PORT");
+	    if (webPort == null || webPort.isEmpty()) {
+	        webPort = "8080";
+	    }
+	    System.setProperty("server.port", webPort);
+	    System.out.println("DB CONNECTION: "+DBConnection.getConnection());
+	    SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ businesskaro-webapp_1/target/classes/public/app/scripts/app.js
@@ -0,0 +1,30 @@
+angular
+  .module('themesApp', [
+    'theme',
+    'theme.demos',
+  ])
+  .config(['$provide', '$routeProvider', function($provide, $routeProvider) {
+    'use strict';
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/views/index.html',
+        resolve: {
+          loadCalendar: ['$ocLazyLoad', function($ocLazyLoad) {
+            return $ocLazyLoad.load([
+              'bower_components/fullcalendar/fullcalendar.js',
+            ]);
+          }]
+        }
+      })
+      .when('/:templateFile', {
+        templateUrl: function(param) {
+          return 'app/views/' + param.templateFile + '.html';
+        }
+      })
+      .when('#', {
+        templateUrl: 'app/views/index.html',
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
