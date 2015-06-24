ad11205e79d1045e48bbfd0aaf0e13121c8a8c64
anirban <anirban.kundu1981@gmail.com>
2015-06-23 20:30:27 -0700

    removing the unwanted jar, adding the app in public folder

--- src/main/java/com/businesskaro/ApplicationConfiguration.java
+++ src/main/java/com/businesskaro/ApplicationConfiguration.java
@@ -1,17 +1,23 @@
 package com.businesskaro;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class ApplicationConfiguration extends WebMvcConfigurerAdapter {
 
 	private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
-			"classpath:/META-INF/resources/", "classpath:/resources/", "classpath:/public/" };
+        "classpath:/META-INF/resources/", "classpath:/resources/",
+        "classpath:/public/app/" };
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/**").addResourceLocations(
 				CLASSPATH_RESOURCE_LOCATIONS);
 	}
+	@Override
+    public void addViewControllers(ViewControllerRegistry registry) { 
+        registry.addViewController("/").setViewName("forward:/index.html") ;
+    }
 }
